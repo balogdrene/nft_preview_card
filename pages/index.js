311e5d09c398ccb2e61ec1fbff1956fa1b5a0501
Rene Balog-Dutombe <balogdrene@gmail.com>
2022-06-10 12:25:25 +0200

    production build

--- pages/index.js
+++ pages/index.js
@@ -25,6 +25,7 @@ export default function Home() {
             <div>
               <div className={styles.image_container}>
                 <Image
+                  alt=""
                   src="/image-equilibrium.jpg"
                   width="280px"
                   height="280px"
@@ -36,12 +37,12 @@ export default function Home() {
                     height="48"
                     xmlns="http://www.w3.org/2000/svg"
                   >
-                    <g fill="none" fill-rule="evenodd">
+                    <g fill="none" fillRule="evenodd">
                       <path d="M0 0h48v48H0z" />
                       <path
                         d="M24 9C14 9 5.46 15.22 2 24c3.46 8.78 12 15 22 15 10.01 0 18.54-6.22 22-15-3.46-8.78-11.99-15-22-15Zm0 25c-5.52 0-10-4.48-10-10s4.48-10 10-10 10 4.48 10 10-4.48 10-10 10Zm0-16c-3.31 0-6 2.69-6 6s2.69 6 6 6 6-2.69 6-6-2.69-6-6-6Z"
                         fill="#FFF"
-                        fill-rule="nonzero"
+                        fillRule="nonzero"
                       />
                     </g>
                   </svg>
@@ -85,6 +86,7 @@ export default function Home() {
                 <hr />
                 <div className="align-center">
                   <Image
+                    alt=""
                     src="/image-avatar.png"
                     width="25px"
                     height="25px"
